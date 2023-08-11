c81e96f5ac6603e4fed41fa0c8ddea74b14fe7fe
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2023-08-11 13:25:38 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import Input from "./Components/InputField";
-import Enteries from "./Components/Enteries";
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import Input from './Components/InputField';
+import Enteries from './Components/Enteries';
 
-
-// Get data from local storage
-const getLocalItems = () =>{
-  let list = localStorage.getItem('lists');
-  
-
-  if (list) {
-    return JSON.parse(localStorage.getItem('lists'));
-  } else {
-    return [];
-  }
-}
-
-function App() {
-  const [todos,setTodos] = useState(getLocalItems());
-
-  // Set Data From Local Storage
-  useEffect(() => {
-    localStorage.setItem('lists',JSON.stringify(todos))
-}, [todos]);
-
-
-
-// Remove Item Functionality
-const removeToDO = (id) => {
-  const newTodos = todos.filter((todo) => todo.id !== id);
-  setTodos(newTodos);
+// Set the data in local location 
+const getLocalItems = () => {
+  const list = localStorage.getItem('lists');
+  return list ? JSON.parse(list) : [];
 };
 
+function App() {
+  const [todos, setTodos] = useState(getLocalItems());
 
-  //Edit functionality 
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(todos));
+  }, [todos]);
+
+
+  // Remove Items
+  const removeToDo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  // Update or edit in list
   const updateTodo = (id, updatedItem) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, item: updatedItem } : todo
@@ -40,74 +30,45 @@ const removeToDO = (id) => {
     setTodos(updatedTodos);
   };
 
-  // Add Item Functionality
+  // Add Items And remove duplicate entries
   const addToDoHandler = (item) => {
-    
-    setTodos(
-      [...todos,
+    const isDuplicate = todos.some((todo) => todo.item === item);
+
+    if (!isDuplicate) {
+      setTodos([
+        ...todos,
         {
           item,
           time: new Date().toLocaleDateString(),
-          id: Date.now()
-        }
-      ]
-    )
-  }
-  // Enter Event Handler
-  const handleKeyPress = (event) => {
-    // if(event.key === "Enter"){
-    //   addToDoHandler(event.target.value);
-    //   event.target.value = "";
-    // }
-
-    if (event.key === "Enter" && event.target.value.trim() !== "") {
-      addToDoHandler(event.target.value);
-      event.target.value = "";
+          id: Date.now(),
+        },
+      ]);
     }
-    
-  }
+  };
 
-  // useEffect(() => {
-  //   window.addEventListener("keydown", handleKeyPress);
-  //   return () => {
-  //     window.removeEventListener("keydown",handleKeyPress)
-  //   };
-  // }, [todos]);
+  // Add when click on enter button
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && event.target.value.trim() !== '') {
+      addToDoHandler(event.target.value);
+      event.target.value = '';
+    }
+  };
 
-    useEffect(() => {
-      window.addEventListener("keydown", handleKeyPress);
-      return () => {
-        window.removeEventListener("keydown", handleKeyPress);
-      };
-    }, []);
-    
-
-
-  // Duplicate Enteries remove
-
-  // useEffect(() => {
-  //   const updateTodos = todos.filter((todo, index) =>
-  //     index === todos.findIndex(t => t.item === todo.item)
-  //   );
-
-  //   setTodos(updateTodos);
-  // }, [todos]);
-
-    useEffect(() => {
-      const updatedTodos = todos.filter((todo, index) =>
-        index === todos.findIndex(t => t.item === todo.item)
-      );
-      setTodos(updatedTodos);
-    }, []);
-  
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
   return (
-    <div className="bg-[#350435] h-screen p-3 flex">
-      <div className="w-[95%] h-[60vh] sm:w-[50%] sm:h-[80vh] shadow-2xl rounded bg-[#FFE6FC] m-auto">
-        <h1 className="text-center font-bold text-white text-xl bg-[#913891] p-2 sm:text-2xl sm:p-3 xl:text-4xl">ToDo List </h1>
-        <Input handler = {addToDoHandler} />
-        <Enteries data = {todos} removeHandler={removeToDO} updateTodo= {updateTodo}/>
-
+    <div className='bg-[#350435] h-screen p-3 flex'>
+      <div className='w-[95%] h-[60vh] sm:w-[50%] sm:h-[80vh] shadow-2xl rounded bg-[#FFE6FC] m-auto'>
+        <h1 className='text-center font-bold text-white text-xl bg-[#913891] p-2 sm:text-2xl sm:p-3 xl:text-4xl'>
+          ToDo List
+        </h1>
+        <Input handler={addToDoHandler} />
+        <Enteries data={todos} removeHandler={removeToDo} updateTodo={updateTodo} />
       </div>
     </div>
   );
