b64fe661a86d3b1b358edd3c86f02316cec25e78
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2023-08-11 13:27:35 +0500

    Updated

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,43 +1,31 @@
-import React, {useRef} from 'react'
+
+import React, { useRef } from 'react';
 
 export default function Input(props) {
-  // Ref use as a ID in react
   const inputBox = useRef();
-  const AddEnteries = () =>{
-    props.handler(inputBox.current.value)
-        inputBox.current.value = "";
-  }
 
-  // const emptyInput = () => {
-  //   let inputText = document.getElementById('text').value; // Get the value of the input field
-  //   const inputRegex = /^\w[A-Za-z]$/;
-  
-  //   if (!inputRegex.test(inputText)) {
-  //     return "Enter a valid text";
-  //   } else {
-  //     return AddEnteries(); 
-    
-  //   }
-  // };
-  
-
- 
+  const addEntries = () => {
+    const inputValue = inputBox.current.value.trim();
+    if (inputValue !== '') {
+      props.handler(inputValue);
+      inputBox.current.value = '';
+    }
+  };
 
   return (
     <div className='p-3 flex justify-around'>
-      <form className='w-[100%]'>
-      <input type="text" name="text" id="text" placeholder='Enter Data Here ...' 
-      className='p-2 pr-2 text-sm border-[#D70FC6] bg-[#FFE6FC] w-[97%]
-       focus:bg-[#FDF3FD] 
-      focus:outline-none blur:bg-[#FFE6FC] sm:p-3 border sm:text-xl'  ref={inputBox} 
-      /></form>
-    
-      <div className='cursor-pointer w-[40px] h-[40px] bg-[#D70FC6] text-white
-       rounded-[100%] flex justify-center items-center sm:w-[50px] 
-       sm:h-[50px]' onClick={AddEnteries}>
-      <i className="fa-solid fa-plus sm:fa-2xl"></i>
+      <input
+        type='text'
+        placeholder='Enter Data Here ...'
+        className='p-2 pr-2 text-sm border-[#D70FC6] bg-[#FFE6FC] w-[97%] focus:bg-[#FDF3FD] focus:outline-none blur:bg-[#FFE6FC] sm:p-3 border sm:text-xl'
+        ref={inputBox}
+      />
+      <div
+        className='cursor-pointer w-[40px] h-[40px] bg-[#D70FC6] text-white rounded-[100%] flex justify-center items-center sm:w-[50px] sm:h-[50px]'
+        onClick={addEntries}
+      >
+        <i className='fa-solid fa-plus sm:fa-2xl'></i>
       </div>
     </div>
-    
-  )
+  );
 }
