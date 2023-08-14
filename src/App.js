0a94e60aac1a2d273e14cb2d6fb89208445e6d19
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2023-08-14 20:28:57 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Input from './Components/InputField';
+import Enteries from './Components/Enteries';
 
-import Input from "./Components/InputField";
-import Enteries from "./Components/Enteries";
-import React, {useState, useEffect} from 'react'
-
-
-// Get data from local storage
-const getLocalItems = () =>{
-  let list = localStorage.getItem('lists');
-  
+const getLocalItems = () => {
+  const list = localStorage.getItem('lists');
 
   if (list) {
-    return JSON.parse(localStorage.getItem('lists'));
+    return JSON.parse(list);
   } else {
     return [];
   }
-}
-
-function App() {
-  const [todos,setTodos] = useState(getLocalItems());
-
-  // Set Data From Local Storage
-  useEffect(() => {
-    localStorage.setItem('lists',JSON.stringify(todos))
-}, [todos]);
-
-
-
-// Remove Item Functionality
-const removeToDO = (id) => {
-  const newTodos = todos.filter((todo) => todo.id !== id);
-  setTodos(newTodos);
-
 };
 
+function App() {
+  const [todos, setTodos] = useState(getLocalItems());
 
-  //Edit functionality 
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(todos));
+  }, [todos]);
+
+  const removeToDO = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   const updateTodo = (id, updatedItem) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, item: updatedItem } : todo
@@ -42,91 +31,60 @@ const removeToDO = (id) => {
     setTodos(updatedTodos);
   };
 
-  // Add Item Functionality
-  const addToDoHandler = (item) => {
-    
-    setTodos(
-      [...todos,
+  const addToDoHandler = useCallback(
+    (item) => {
+      setTodos([
+        ...todos,
         {
           item,
           time: new Date().toLocaleDateString(),
-          id: Date.now()
-        }
-      ]
-    )
-
-    
-  }
-  // Enter Event Handler
-  // const handleKeyPress = (event) => {
-    // if(event.key === "Enter"){
-    //   addToDoHandler(event.target.value);
-    //   event.target.value = "";
-    // }
-
-  //   if (event.key === "Enter" && event.target.value.trim() !== '') {
-  //     addToDoHandler(event.target.value);
-  //     event.target.value = '';
-  //   }
-    
-  // }
-  // useEffect(() => {
-  //   window.addEventListener("keydown", handleKeyPress);
-  //   return () => {
-  //     window.removeEventListener("keydown",handleKeyPress)
-  //   };
-  // }, [todos]);
-
-  // useEffect(() => {
-  //   window.addEventListener("keydown", handleKeyPress);
-  //   return () => {
-  //     window.removeEventListener("keydown", handleKeyPress);
-  //   };
-  // }, []);
+          id: Date.now(),
+        },
+      ]);
+    },
+    [todos]
+  );
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === "Enter" && event.target.value.trim() !== "") {
+      if (event.key === 'Enter' && event.target.value.trim() !== '') {
         addToDoHandler(event.target.value);
-        event.target.value = "";
+        event.target.value = '';
       }
     };
-  
-    window.addEventListener("keydown", handleKeyDown);
+
+    window.addEventListener('keydown', handleKeyDown);
     return () => {
-      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, [addToDoHandler]);
-  
-  
-    
 
-
-  // Duplicate Enteries remove
+  // useEffect(() => {
+  //   const updateTodos = todos.filter(
+  //     (todo, index) => index === todos.findIndex((t) => t.item === todo.item)
+  //   );
+  //   setTodos(updateTodos);
+  // }, []);
 
   useEffect(() => {
-    const updateTodos = todos.filter((todo, index) =>
-      index === todos.findIndex(t => t.item === todo.item)
+    const updateTodos = todos.filter(
+      (todo, index) => index === todos.findIndex((t) => t.item === todo.item)
     );
-
-    setTodos(updateTodos);
+    
+    if (updateTodos.length !== todos.length) {
+      setTodos(updateTodos);
+    }
   }, [todos]);
-
-    // useEffect(() => {
-    //   const updatedTodos = todos.filter((todo, index) =>
-    //     index === todos.findIndex(t => t.item === todo.item)
-    //   );
-    //   setTodos(updatedTodos);
-    // }, []);
   
 
   return (
     <div className="bg-[#350435] h-screen p-3 flex">
       <div className="w-[95%] h-[60vh] sm:w-[50%] sm:h-[80vh] shadow-2xl rounded bg-[#FFE6FC] m-auto">
-        <h1 className="text-center font-bold text-white text-xl bg-[#913891] p-2 sm:text-2xl sm:p-3 xl:text-4xl">ToDo List </h1>
-        <Input handler = {addToDoHandler} />
-        <Enteries data = {todos} removeHandler={removeToDO} updateTodo= {updateTodo}/>
-
+        <h1 className="text-center font-bold text-white text-xl bg-[#913891] p-2 sm:text-2xl sm:p-3 xl:text-4xl">
+          ToDo List
+        </h1>
+        <Input handler={addToDoHandler} />
+        <Enteries data={todos} removeHandler={removeToDO} updateTodo={updateTodo} />
       </div>
     </div>
   );
